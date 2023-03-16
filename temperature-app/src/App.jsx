051ea49f506ca79862959e7c619a55072d7d13e2
dachi-meparishvili <dachi.meparishvili7@gmail.com>
2023-03-16 16:48:18 +0400

    second commit

--- temperature-app/src/App.jsx
+++ temperature-app/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App()  {
+  // თუ ტემპერატურა 0ზე ნაკლებია, temperature დივის ბეგქრაუნდ ფერი იყოს ლურჯი, თუ მეტია 0ზე და ნაკლებია 15ზე იყოს მწვანე, 15დან 30მდე იყოს ყვითელი, ხოლო 30ის ზევით იყოს წითელი
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [tempColor, setTempColor] = useState("")
+
+  const handleIncrement = () => {
+    setTemperatureValue(temperatureValue + 1)
+    asld()
+  }
+
+  const handleDecrement = () => {
+    setTemperatureValue(temperatureValue - 1)
+    asld()
+  }
+
+  const asld = () => {
+    if(temperatureValue <= 0) {
+      setTempColor("blue")
+    } else if(temperatureValue > 0 && temperatureValue < 15) {
+      setTempColor("green")
+    } else if(temperatureValue >= 15 && temperatureValue < 30) {
+      setTempColor("orange")
+    } else if(temperatureValue >= 30) {
+      setTempColor("red")
+    }
+  }
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${tempColor}`}>{temperatureValue}⁰C</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="button-container">
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
